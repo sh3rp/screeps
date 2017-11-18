70483eafc982d2d48eb242173612c150191d3461
Shepherd Kendall <s@kndl.org>
2017-11-18 15:07:10 -0500

    added password read from env var

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,17 @@
-var prompt = require('prompt');
 
 module.exports = function(grunt) {
-
     grunt.loadNpmTasks('grunt-screeps');
-    grunt.loadNpmTasks('grunt-prompt');
 
     grunt.initConfig({
         screeps: {
             options: {
                 email: 's@kndl.org',
+                password: process.env.SCREEPS_PASSWORD,
                 branch: 'test',
                 ptr: false
             },
             dist: {
                 src: ['src/*.js']
-            }
-        },
-        prompt: {
-            target: {
-                options: {
-                    questions: [
-                        {
-                            config: 'screeps.options.password',
-                            type: 'password',
-                            message: 'Screeps password:',
-                            default: ''
-                        }
-                    ]
-                }
             }
         }
     });
