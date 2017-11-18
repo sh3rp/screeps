2432b7b230fec4c3b715ec028f7679bb0c2f8f4d
Shepherd Kendall <s@kndl.org>
2017-11-18 14:39:34 -0500

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,32 @@ var prompt = require('prompt');
 
 module.exports = function(grunt) {
 
-    var password = '';
-    prompt.start();
-    prompt.get(['password'],function(err, result) {
-        password = result.password; 
-    });
-
     grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-prompt');
 
     grunt.initConfig({
         screeps: {
             options: {
                 email: 's@kndl.org',
-                password: password,
                 branch: 'test',
                 ptr: false
             },
             dist: {
                 src: ['src/*.js']
+            }
+        },
+        prompt: {
+            target: {
+                options: {
+                    questions: [
+                        {
+                            config: 'screeps.options.password',
+                            type: 'password',
+                            message: 'Screeps password:',
+                            default: ''
+                        }
+                    ]
+                }
             }
         }
     });
