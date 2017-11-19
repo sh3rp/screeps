a3f53b194945d25ca0053437436835483230c21a
Shepherd Kendall <s@kndl.org>
2017-11-19 07:24:10 -0500

    wip

--- src/builder.js
+++ src/builder.js
@@ -9,6 +9,7 @@
  
 var Builder = function() {
     this.MAX_BUILDERS = 3;
+    this.traveled = {};
 };
 
 Builder.prototype.spawnBuilders = function() {
@@ -29,6 +30,27 @@ Builder.prototype.getBuilders = function() {
     }
     return builders;
 };
+
+Builder.prototype.mark = function(creep) {
+    var terrain = this.terrain(creep);
+    if(this.traveled[terrain] == undefined) {
+        this.traveled[terrain] = {};
+    }
+    var coords = this.coords(creep);
+    if(this.traveled[terrain][coords] == undefined) {
+        this.traveled[terrain][coords] = 1
+    } else {
+        this.traveled[terrain][coords] = this.traveled[terrain][coords] + 1;
+    }
+}
+
+Builder.prototype.terrain = function(creep) {
+    return creep.pos.lookFor(LOOK_TERRAIN);
+}
+
+Builder.prototype.coords = function(creep) {
+    return creep.pos.x + "." + creep.pos.y;
+}
     
 
 module.exports = Builder;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 var Builder = require('builder');
 
 module.exports.loop = function () {
-    var b = new Builder();
+    if(global.builder == undefined) {
+        global.builder = new Builder();
+        console.log("Created new builder");
+    }
+    var b = global.builder;
+    console.log("Builder = "+ JSON.stringify(b));
     b.spawnBuilders();
-    //console.log(builder.getBuilders());
     spawnHarvesters();
     var harvesters = getHarvesters();
-    //console.log(JSON.stringify(highTrafficSwamp()));
     for (var idx in harvesters) {
         var creep = harvesters[idx];
-        markSwamp(getTerrain(creep));
+        builder.mark(creep);
         if(creep.memory['state'] == HARVEST) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
@@ -45,40 +48,6 @@ var MAX_HARVESTERS = 12;
 
 var HARVEST = 1;
 var DISPENSE = 2;
-
-function markSwamp(tPos) {
-    if(tPos["t"] == "swamp") {
-        var key = tPos["x"] + "." + tPos["y"];
-        if(Memory.highTrafficSwamp == undefined) {
-            Memory.highTrafficSwamp = {};
-        }
-        if(key in Memory.highTrafficSwamp) {
-            Memory.highTrafficSwamp[key] = Memory.highTrafficSwamp[key] + 1;
-        } else {
-            Memory.highTrafficSwamp[key] = 1;
-        }
-    }
-}
-
-function highTrafficSwamp() {
-    return Memory.highTrafficSwamp;
-}
-
-function getTerrain(creep) {
-    var looked = creep.pos.look();
-    var x = creep.pos.x;
-    var y = creep.pos.y;
-    
-    for(var idx in looked) {
-        if(looked[idx]['type'] == 'terrain') {
-            return {
-                "t":looked[idx]['terrain'],
-                "x":x,
-                "y":y
-            };
-        }
-    }
-}
 
 function getHarvesters() {
     var harvesters = [];
